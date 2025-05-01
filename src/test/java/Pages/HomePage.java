9189f2a2d567c56ee3a4ec22afa936cdaf0991e1
AnnaKuruvilla <annakuruvilla88@gmail.com>
2025-05-01 14:11:52 +0400

    1st commit

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -7,27 +7,16 @@ import static Tests.Selenium_test.driver;
 
 public class HomePage {
 
-
     public static String hambur_menu_xpath="//*[@id=\"menuToggle\"]/input" ;
-
-
     public static String online_products ="//*[@id=\"menu\"]/a[3]/li" ;
-
-
     public static void Click_hamburger_menu()
-
     {
         driver.findElement(By.xpath(hambur_menu_xpath)).click();
     }
-
     public static void Click_online_product() throws InterruptedException
     {
 
         Thread.sleep(2000);
         driver.findElement(By.xpath(online_products)).click();
-
-
     }
-
-
 }
